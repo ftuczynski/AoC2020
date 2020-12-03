6c99e4ac3f05979b608cc2a8011d020c2766501e
Filip Tuczyński <ftuczynski@gmail.com>
2020-12-03 14:04:48 +0100

    Day 3

---
+++ AdventD03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AdventD03 {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                var input = File.ReadAllLines("input.txt");
+                var slop = new Slope(input);
+
+                // PART 1
+                Console.WriteLine("Part 1: " + slop.DownSlope(1, 3));
+
+                // PART 2
+                int[,] slopes = { { 1, 1 }, { 1, 3 }, { 1, 5 }, { 1, 7 }, { 2, 1 } };
+                double ans = 1;
+                for (int i = 0; i < slopes.GetLength(0); i++)
+                    ans *= slop.DownSlope(slopes[i, 0], slopes[i, 1]);
+                Console.WriteLine("Part 2: " + ans);
+            }
+            catch (IOException e) {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ AdventD03/Slope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace AdventD03 {
+    class Slope {
+        public List<char[]> map { get; }
+
+        public Slope(string[] input) {
+            map = new List<char[]>();
+            for(int i = 0; i < input.Length; i++) {
+                map.Add(input[i].ToCharArray());
+            }
+        }
+
+        public int DownSlope(int down, int right) {
+            int treeCounter = 0;
+            int j = 0;
+            for(int i = 0; i < map.Count; i+=down) {
+                if (map[i][j] == '#')
+                    treeCounter++;
+                j = (j + right) % map[i].Length;
+            }
+            return treeCounter;
+        }
+    }
+}
