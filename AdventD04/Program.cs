52d4d4eb9ef5f8c0fa7f3b6c70043e760d1cb62f
Filip Tuczyński <ftuczynski@gmail.com>
2020-12-04 13:31:58 +0100

    Day 4 Part1

---
+++ AdventD04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventD04 {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                var passports = File.ReadAllText("input.txt")
+                    .Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None)
+                    .Select(x => x.Replace(Environment.NewLine, " "));
+
+                // PART 1
+                int count = passports
+                    .Where(x => DoesPassportHaveAllFields(x))
+                    .Count();
+
+                Console.WriteLine("Part 1:" + count);
+
+                // PART 2
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+        private static bool DoesPassportHaveAllFields(string passport) {
+            var fields = new string[] { "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:" };
+            return fields.All(x => passport.Contains(x));
+        }
+    }
+}
