1b0d7cc16928cb6e8ac3c251858b7116e7c986c8
Filip Tuczyński <ftuczynski@gmail.com>
2020-12-08 13:14:27 +0100

    Day 7

---
+++ AdventD07/Bag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventD07 {
+    class Bag {
+        private static readonly Regex colorRegex = new Regex(@"[\w]+\s+[\w]+", RegexOptions.None);
+        private static readonly Regex bagsRegex = new Regex(@"(\d+) ([\w]+\s+[\w]+)", RegexOptions.None);
+        public string Name { get; }
+        public IDictionary<string, int> Content { get; }
+
+        public Bag(string line) {
+            Name = colorRegex.Match(line).Value;
+            Content = bagsRegex.Matches(line)
+                .Cast<Match>()
+                .ToDictionary(
+                x => x.Groups[2].Value,
+                x => int.Parse(x.Groups[1].Value)
+                );
+        }
+        public bool ContainsBag(string name, List<Bag> bags) {
+            return Content.Keys.Any(x => x.Equals(name) || GetBag(x, bags).ContainsBag(name, bags));
+        }
+        public static Bag GetBag(string key, List<Bag> bags) {
+            return bags.First(x => x.Name.Equals(key));
+        }
+        public int HowManyBags(List<Bag> bags) {
+            return Content.Sum(x => x.Value * (GetBag(x.Key, bags).HowManyBags(bags) + 1));
+        }
+    }
+}
---
+++ AdventD07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventD07 {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                var input = File.ReadAllLines("input.txt");
+                var bags = input.Select(x => new Bag(x)).ToList();
+
+                // PART 1
+                var ans1 = bags.Count(x => x.ContainsBag("shiny gold", bags));
+                Console.WriteLine($"Part 1: {ans1}");
+
+                // PART 2
+                var ans2 = Bag.GetBag("shiny gold", bags).HowManyBags(bags);
+                Console.WriteLine($"Part 2: {ans2}");
+            }
+            catch (IOException e) {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
